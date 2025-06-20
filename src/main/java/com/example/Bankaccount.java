9653f421e097f3d70c9303a9f0d622d9956c4438
waqar khan <waqarkhan9@yahoo.com>
2025-06-20 13:09:33 -0700

    new code added

---
+++ src/main/java/com/example/Bankaccount.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import java.util.ArrayList;
+
+public class Bankaccount {
+    int acc_no;
+    String name;
+
+    public Bankaccount() {
+        this.acc_no = acc_no;
+        this.name = name;
+    }
+
+    public void getAccountDetails() {
+        System.out.println("Account Number : " + acc_no + "Name : " + name);
+    }
+
+}
